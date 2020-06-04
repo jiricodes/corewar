c2e5345c356541e14fbc24514f157944b14b903b
Jiri Novotny <hello@jiricodes.com>
2020-06-04 13:42:07 +0300

    created simple asm init. if compiled, it opens source file into source_fd and opens or creates filename.cor file with file decriptor in core_fd. Currently doesn't clean memory on error exit, as I've forgotten how my function works exactly xD #lunchbreak

--- sources/asm/cleanup.c
+++ sources/asm/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 11:20:45 by jnovotny          #+#    #+#             */
-/*   Updated: 2020/06/04 13:06:49 by jnovotny         ###   ########.fr       */
+/*   Updated: 2020/06/04 13:21:05 by jnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	clear_t_asm(t_asm *object)
 			ft_error_exit("Failed to close source file", NULL, NULL);
 	if (object->core_fd > 2)
 		if (close(object->core_fd) < 0)
-			t_error_exit("Failed to close target file", NULL, NULL);
+			ft_error_exit("Failed to close target file", NULL, NULL);
 	free(object);
 }
--- sources/asm/init.c
+++ sources/asm/init.c
@@ -6,7 +6,7 @@
 /*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 10:57:41 by jnovotny          #+#    #+#             */
-/*   Updated: 2020/06/04 13:03:41 by jnovotny         ###   ########.fr       */
+/*   Updated: 2020/06/04 13:38:45 by jnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,13 @@ t_asm	*intialize_asm(char *filename)
 		ft_error_exit("Malloc at initialize_asm", NULL, NULL);
 	target_file = filename_pars(filename, SRC_TYPE, TRGT_TYPE);
 	if (!target_file)
-		ft_error_exit("Given file is of incorrect type", core, clear_t_asm);
+		ft_error_exit("Given file is of incorrect type", NULL, NULL);
+	ft_printf("Attempting to open  source file %s\n", filename);
 	core->source_fd = open(filename, O_RDONLY);
 	if (core->source_fd < 0)
-		ft_error_exit("Failed to open given source file", core, clear_t_asm);
-	core->core_fd = open(target_file, O_RDWR);
+		ft_error_exit("Failed to open given source file", NULL, NULL);
+	core->core_fd = open(target_file, O_RDWR | O_CREAT, 0600);
 	if (core->core_fd < 0)
-		ft_error_exit("Failed to open given source file", core, clear_t_asm);
+		ft_error_exit("Failed to open given target file", NULL, NULL);
 	return (core);
 }
