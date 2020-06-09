a859830225b06a2886c288c23b46db2119bf4bfe
Jiri Novotny <hello@jiricodes.com>
2020-06-09 11:14:44 +0300

    clean memory in asm.c

--- sources/asm/cleanup.c
+++ sources/asm/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 11:20:45 by jnovotny          #+#    #+#             */
-/*   Updated: 2020/06/09 11:03:43 by jnovotny         ###   ########.fr       */
+/*   Updated: 2020/06/09 11:10:53 by jnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,9 @@ void	clear_t_asm(void *object)
 	if (((t_asm*)object)->core_fd > 2)
 		if (close(((t_asm*)object)->core_fd) < 0)
 			ft_error_exit("Failed to close target file", NULL, NULL);
+	if (((t_asm*)object)->champ_name)
+		free(((t_asm*)object)->champ_name);
+	if (((t_asm*)object)->champ_comment)
+		free(((t_asm*)object)->champ_comment);
 	free(object);
 }
