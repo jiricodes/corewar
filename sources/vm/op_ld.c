2bf5fdc53900dcbc8eac9c2a5fc1be3ae710f187
Artemso <solopovtema@gmail.com>
2020-07-17 13:05:16 +0300

    var changes

--- sources/vm/op_ld.c
+++ sources/vm/op_ld.c
@@ -6,7 +6,7 @@
 /*   By: asolopov <asolopov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/17 12:48:07 by asolopov          #+#    #+#             */
-/*   Updated: 2020/07/17 12:50:28 by asolopov         ###   ########.fr       */
+/*   Updated: 2020/07/17 13:02:04 by asolopov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	op_ld(int8_t *rawcode, t_car *car)
 
 	args = init_args("ld");
 	code = rawcode;
-	if (!read_arg_type(args, (code + 1)[0]))
+	if (!read_arg_type(args, (code + OP_BYTE)[0]))
 	{
 		get_jump(car, args);
 		return ;
@@ -28,14 +28,14 @@ void	op_ld(int8_t *rawcode, t_car *car)
 	if (args->arg_types[0] == T_DIR)
 	{
 		args->arg[0] = decode((uint8_t *)code, args->t_dir_size);
-		args->arg[1] = decode((uint8_t *)code + args->t_dir_size, 1);
-		car->reg[args->arg[1]] = decode((uint8_t *)(rawcode + args->arg[0]), 4);
+		args->arg[1] = decode((uint8_t *)code + args->t_dir_size, TREG_BYTE);
+		car->reg[args->arg[1]] = decode((uint8_t *)(rawcode + args->arg[0]), REGSIZE);
 	}
 	else if (args->arg_types[0] == T_IND)
 	{
-		args->arg[0] = decode((uint8_t *)code, 2);
-		args->arg[1] = decode((uint8_t *)code + 2, 1);
-		car->reg[args->arg[1]] = decode((uint8_t *)(rawcode + args->arg[0]), 4) % IDX_MOD;
+		args->arg[0] = decode((uint8_t *)code, TIND_BYTE);
+		args->arg[1] = decode((uint8_t *)code + TIND_BYTE, TREG_BYTE);
+		car->reg[args->arg[1]] = decode((uint8_t *)(rawcode + args->arg[0]), REGSIZE) % IDX_MOD;
 	}
 	car->carry = (car->reg[args->arg[1]]) ? 0 : 1;
 	get_jump(car, args);
