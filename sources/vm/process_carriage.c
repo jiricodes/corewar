b223f95411f559ac23bbf5e8ae2a8ba848580fbd
Artemso <solopovtema@gmail.com>
2020-07-28 18:05:53 +0300

    process carriage

--- sources/vm/process_carriage.c
+++ sources/vm/process_carriage.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   process_carriage.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: asolopov <asolopov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/28 06:21:47 by jnovotny          #+#    #+#             */
-/*   Updated: 2020/07/28 06:27:40 by jnovotny         ###   ########.fr       */
+/*   Updated: 2020/07/28 17:36:45 by asolopov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 #include "oplist_cw.h"
 
-static int		if_op(int8_t byte)
+static int		if_op(int8_t byte, t_args *args)
 {
 	int cnt;
 
@@ -21,7 +21,19 @@ static int		if_op(int8_t byte)
 	while (cnt < 16)
 	{
 		if (byte == g_oplist[cnt].opcode)
+		{
+			args->opcount = cnt;
+			args->arg_cnt = g_oplist[cnt].arg_cnt;
+			args->arg_code = g_oplist[cnt].arg_type_code;
+			args->dir_size = g_oplist[cnt].t_dir_size;
+			if (!args->arg_code)
+			{
+				args->arg_types[0] = g_oplist[cnt].arg_type[0];
+				args->arg_types[1] = g_oplist[cnt].arg_type[1];
+				args->arg_types[2] = g_oplist[cnt].arg_type[2];
+			}
 			return (g_oplist[cnt].opcode - 1);
+		}
 		cnt += 1;
 	}
 	return (-1);
@@ -29,12 +41,10 @@ static int		if_op(int8_t byte)
 
 static void		check_operation(t_vm *core, t_car *car)
 {
-	car->op_index = if_op(core->arena[car->pc]);
+	ft_bzero(car->args, sizeof(t_args));
+	car->op_index = if_op(core->arena[car->pc], car->args);
 	if (car->op_index != -1)
-	{
 		car->cooldown = g_oplist[car->op_index].exec_cycles;
-		ft_bzero(car->args, sizeof(t_args));
-	}
 	else
 		car->step = 1;
 }
@@ -57,10 +67,7 @@ void			process_car(t_vm *core, t_car *car)
 	{
 		if (car->op_index != -1)
 		{
-			vm_log(F_LOG, "[%zu]: P%5zu | %s ", core->cycle,\
-				car->id + 1, g_oplist[car->op_index].opname);
 			g_oplist[car->op_index].op(core, car);
-			vm_log(F_LOG, "\n");
 			car->op_index = -1;
 		}
 		car->pc = (car->pc + car->step) % MEM_SIZE;
