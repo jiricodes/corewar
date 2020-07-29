5b06e42a92ebcad157b3f823a03dca3646ac30d2
Jani Mäkelä <58624979+jmakela42@users.noreply.github.com>
2020-07-29 10:44:57 +0300

    Normed
    Using norminette @home. Just in case, check with school norminette.

--- includes/asm.h
+++ includes/asm.h
@@ -18,23 +18,23 @@
 # include "libft.h"
 # include "ft_error.h"
 
-# define SRC_TYPE ".s"
-# define TRGT_TYPE ".cor"
+# define				SRC_TYPE ".s"
+# define				TRGT_TYPE ".cor"
 
 /*
 ** ASM core struct.
 */
 
-typedef struct	s_asm
+typedef struct			s_asm
 {
-	int			source_fd;
-	int			core_fd;
-	char		*target_file;
-	char		*champ_name;
-	char		*champ_comment;
-	int			byte_size;
-	int			line_cnt;
-}				t_asm;
+	int					source_fd;
+	int					core_fd;
+	char				*target_file;
+	char				*champ_name;
+	char				*champ_comment;
+	int					byte_size;
+	int					line_cnt;
+}						t_asm;
 
 /*
 ** ASM parser struct.
@@ -46,99 +46,99 @@ typedef struct	s_asm
 ** the position where argument label points to.
 */
 
-typedef struct	s_operation
+typedef struct			s_operation
 {
-	char		*label;
-	char		*op_name;
-	int			op_code;
-	char    	*arg[3];
-	int			argtypes[3];
-	int     	op_size;
-	int     	t_dir_size;
-	int			arg_type_code;
-	int			position;
-	int			label_pos[3];
-	struct		s_operation *next;
-}				t_operation;
+	char				*label;
+	char				*op_name;
+	int					op_code;
+	char				*arg[3];
+	int					argtypes[3];
+	int					op_size;
+	int					t_dir_size;
+	int					arg_type_code;
+	int					position;
+	int					label_pos[3];
+	struct s_operation	*next;
+}						t_operation;
 
 /*
 ** ASM struct utilities
 */
 
-void	clear_t_asm(void *object);
-char	*filename_pars(const char *source, const char *src_type,\
-		const char *target_type);
-t_asm	*intialize_asm(char *filename);
-int		ft_chrpos(char *str, char c);
-int		is_hex(char *argum);
-char	*x_to_deci(char *argum);
-int		is_special(char *argum, int func);
+void					clear_t_asm(void *object);
+char					*filename_pars(const char *source, const char *src_type,\
+						const char *target_type);
+t_asm					*intialize_asm(char *filename);
+int						ft_chrpos(char *str, char c);
+int						is_hex(char *argum);
+char					*x_to_deci(char *argum);
+int						is_special(char *argum, int func);
 
 /*
 ** ASM File reading
 */
 
-void	read_file(t_asm *core, int source_fd, t_operation **list);
+void					read_file(t_asm *core, int source_fd, t_operation **list);
 
 /*
 ** ASM Parsing
 */
 
-int		lex_parser(t_asm *core, t_operation **list, char *line);
-int		get_size_type(t_operation **list, t_asm *core);
-int		find_position(t_operation **list, t_operation *temp, char *arg);
-void	special_arg_finder(t_operation **head);
-int		arg_math(t_operation **head, t_operation *cur, char *label, int cnt);
-void	get_label_op(t_asm *core, t_operation **list, char *line);
-char	*strjoin_first(char *s1, char *s2);
-char	*remove_trailing_spaces(char *src);
+int						lex_parser(t_asm *core, t_operation **list, char *line);
+int						get_size_type(t_operation **list, t_asm *core);
+int						find_position(t_operation **list, t_operation *temp, char *arg);
+void					special_arg_finder(t_operation **head);
+int						arg_math(t_operation **head, t_operation *cur, char *label, int cnt);
+void					get_label_op(t_asm *core, t_operation **list, char *line);
+char					*strjoin_first(char *s1, char *s2);
+char					*remove_trailing_spaces(char *src);
 
 /*
 ** ASM Error checking
 */
 
-void	check_operation( t_operation *operation, t_asm *core);
-void	match_labels(t_operation **head, int cnt, int pos, int test);
-int		check_label(char *label, t_operation **head);
-int		check_argument(char *argum, t_asm *core);
-int		check_name_cmt(t_asm *core);
-int		check_label_chars(char *label);
-int		check_lastline(int source_fd);
-int		special_arg_check(char *label, t_operation **head);
+void					check_operation(t_operation *operation, t_asm *core);
+void					match_labels(t_operation **head, int cnt, int pos, int test);
+int						check_label(char *label, t_operation **head);
+int						check_argument(char *argum, t_asm *core);
+int						check_name_cmt(t_asm *core);
+int						check_label_chars(char *label);
+int						check_lastline(int source_fd);
+int						special_arg_check(char *label, t_operation **head);
 
 /*
 ** ASM List functions
 */
 
-int     list_append(t_operation **head);
+int						list_append(t_operation **head);
 
 /*
 ** ASM Bytecode write functions
 */
 
-void	write_champ_name(t_asm *core, int fd);
-void	write_champ_comment(t_asm *core, int fd);
-void	write_exec_size(t_asm *core, int fd);
-void	write_null(int fd);
-void	write_magic(int fd);
-void	write_exec_code(t_asm *core, t_operation *op);
-void	write_champion(t_asm *core, t_operation *op);
+void					write_champ_name(t_asm *core, int fd);
+void					write_champ_comment(t_asm *core, int fd);
+void					write_exec_size(t_asm *core, int fd);
+void					write_null(int fd);
+void					write_magic(int fd);
+void					write_exec_code(t_asm *core, t_operation *op);
+void					write_champion(t_asm *core, t_operation *op);
 
 /*
 ** ASM Bytecode write helper functions
 */
 
-int		get_arg_code(t_operation *operation);
-void	write_headers(t_asm *core);
-int		swap_int32(int val);
-int		swap_int16(int val);
+int						get_arg_code(t_operation *operation);
+void					write_headers(t_asm *core);
+int						swap_int32(int val);
+int						swap_int16(int val);
 
 /*
 ** ASM Debug stuff
 ** REMOVE FOR FINAL VERSION
 */
 
-void	print_asmcore(t_asm *core);
-void    print_list(t_operation *list, t_asm *core);
+void					print_asmcore(t_asm *core);
+void					print_list(t_operation *list, t_asm *core);
 
 #endif
