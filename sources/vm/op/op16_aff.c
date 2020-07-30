65f85115245d5dd3fbf258c9ff4f476d1efb5318
Artemso <solopovtema@gmail.com>
2020-07-30 11:11:14 +0300

    aff operation works now :)

--- sources/vm/op/op16_aff.c
+++ sources/vm/op/op16_aff.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op16_aff.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: asolopov <asolopov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/17 15:02:59 by jnovotny          #+#    #+#             */
-/*   Updated: 2020/07/29 18:17:48 by jnovotny         ###   ########.fr       */
+/*   Updated: 2020/07/30 11:08:26 by asolopov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,19 @@
 void	op_aff(t_vm *core, t_car *car)
 {
 	ssize_t	index;
-	int		val;
+	char	val;
 
 	index = car->pc + OP_SIZE;
 	if (read_args(core, car->args, index % MEM_SIZE))
 	{
 		vm_log(core->flags->log, OP_STR, core->cycle, car->id + 1, "aff");
-		val = (char)(car->reg[car->args->arg[0] - 1]);
+		val = (char)(car->reg[car->args->arg[0] - 1] % 256);
 		vm_log(core->flags->log, "%d\n", val);
 		if (core->flags->aff)
+		{
 			ft_putchar(val);
+			ft_putchar('\n');
+		}
 	}
 	get_step(car, car->args);
 }
