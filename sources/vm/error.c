25abb68f7b8241c0abc394ac2abb20df6509e541
Jiri Novotny <hello@jiricodes.com>
2020-08-03 14:36:47 +0300

    usage fix

--- sources/vm/error.c
+++ sources/vm/error.c
@@ -6,7 +6,7 @@
 /*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 14:17:47 by jnovotny          #+#    #+#             */
-/*   Updated: 2020/08/03 14:12:57 by jnovotny         ###   ########.fr       */
+/*   Updated: 2020/08/03 14:36:37 by jnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,16 @@ void	vm_error(char *usr_msg)
 
 void	print_usage(void)
 {
-	ft_printf("Usage:\n");
+	ft_printf("Usage:\n\n");
 	ft_printf("./corewar -[flag] [-n [player_number] [player.cor]] ...\n");
 	ft_printf("\nFlags:\n");
-	ft_printf("-n [number] <player.cor>\tAssign the number to the player");
-	ft_printf("-d [number]\tDumps memory after that cycle. 64 octets");
-	ft_printf("-dump [number]\tDumps memory after that cycle. 32 octets");
-	ft_printf("-v\t\ttoggle visualiser");
-	ft_printf("-a\t\taff support");
-	ft_printf("-log [1 | 2]\t\ttoggle log 1 file, 2 file & stdout");
-	ft_printf("-s\t\tsilent run");
+	ft_printf("-n [number] <player.cor>\tAssign the number to the player\n");
+	ft_printf("-d [number]\t\t\tDumps memory after that cycle. 64 octets\n");
+	ft_printf("-dump [number]\t\t\tDumps memory after that cycle. 32 octets\n");
+	ft_printf("-v\t\t\t\ttoggle visualiser\n");
+	ft_printf("-a\t\t\t\taff support\n");
+	ft_printf("-log [number]\n");
+	ft_printf("\t\t\t\t%d - operations, includes cycle\n", LOG_OPS);
+	ft_printf("\t\t\t\t%d - details\n", LOG_DETAILS);
+	ft_printf("-s\t\t\t\tsilent run\n");
 }
