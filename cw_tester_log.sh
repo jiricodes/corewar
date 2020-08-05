093924cd30fd974810fa00f95fa71cd68523f743
Jiri Novotny <hello@jiricodes.com>
2020-08-05 13:33:34 +0300

    tester improvements

--- cw_tester_log.sh
+++ cw_tester_log.sh
@@ -1,11 +1,14 @@
 rm -f cw_diff_log dump_orig dump_us dump_orig_parsed
-./corewar $1 $2 $3 $4 -s -log 1 > dump_us
-./resources/corewar -v 6 $1 $2 $3 $4 > dump_orig
+echo "\nOur CW"
+time ./corewar $1 $2 $3 $4 -s -log 1 > dump_us
+echo "\nOriginal CW"
+time ./resources/corewar -v 6 $1 $2 $3 $4 > dump_orig
 ./parse_orig.py > dump_orig_parsed
 DIFF=$(diff dump_orig_parsed dump_us)
+echo ""
 if [ "$DIFF" != "" ]
 then
 	echo "$DIFF" >> cw_diff_log
-	echo "Difference found and saved in cw_diff_log"
+	echo "\033[31mDifference found and saved in cw_diff_log\033[0m"
 fi
-echo "Done"
+echo "\033[32mDone\033[0m"
