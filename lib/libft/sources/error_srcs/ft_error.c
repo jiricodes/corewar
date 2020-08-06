d4e19cda8dbc6bf3c4a5d02334e062983d99bcb6
jmakela42 <jmakela@student.hive.fi>
2020-08-06 22:21:04 +0300

    im a potato

--- lib/libft/sources/error_srcs/ft_error.c
+++ lib/libft/sources/error_srcs/ft_error.c
@@ -5,6 +5,6 @@ void	ft_error(char *user_msg)
 	if (errno != 0)
 		perror("SYSTEM ERROR MESSAGE: ");
 	if (user_msg)
-		ft_printf("%{RED}%{BOLD}USER ERROR MESSAGE%{EOC}: %s\n", user_msg);
+		ft_dprintf(2, "%{RED}%{BOLD}USER ERROR MESSAGE%{EOC}: %s\n", user_msg);
 	exit(-1);
 }
