4e257876286fb8b63d942f047b0e475bdf510981
Artemso <solopovtema@gmail.com>
2020-08-12 21:01:33 +0300

    file open changes

--- sources/asm/to_bytecode.c
+++ sources/asm/to_bytecode.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   to_bytecode.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: asolopov <asolopov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 12:05:11 by asolopov          #+#    #+#             */
-/*   Updated: 2020/07/13 13:38:57 by jnovotny         ###   ########.fr       */
+/*   Updated: 2020/08/12 20:58:30 by asolopov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	write_champion(t_asm *core, t_operation *op)
 {
-	core->core_fd = open(core->target_file, O_RDWR | O_CREAT, 0600);
+	core->core_fd = open(core->target_file, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (core->core_fd < 0)
 		ft_error_exit("Open Error on target file", (void *)core, clear_t_asm);
 	write_headers(core);
--- sources/deasm/deasm.c
+++ sources/deasm/deasm.c
@@ -6,7 +6,7 @@
 /*   By: asolopov <asolopov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 12:42:40 by asolopov          #+#    #+#             */
-/*   Updated: 2020/07/30 10:33:42 by asolopov         ###   ########.fr       */
+/*   Updated: 2020/08/12 21:01:03 by asolopov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int		main(int argc, char **argv)
 	{
 		core = initialize_deasm(argv[1]);
 		read_file(core, core->source_fd);
-		core->target_fd = open(core->target_file, O_RDWR | O_CREAT, 0600);
+		core->target_fd = open(core->target_file,\
+				O_RDWR | O_TRUNC | O_CREAT, 0600);
 		if (core->target_fd < 0)
 			ft_error_exit("Couldn't open target", (void *)core, clear_t_deasm);
 		decode_exec(core, core->raw_code);
